4d9594146fd5e32c4517fc741e15bf70debeea0d
Ryan Detzler <detzlerryan@gmail.com>
2021-10-05 11:51:05 -0400

    created customuser model with username, checked_out_books and library_card_number field. added custom user to admin.py

--- custom_user/models.py
+++ custom_user/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from book.models import Book
+import random
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    def random_card_number():
+        return str(random.randint(100000, 999999))
+    
+    username = models.CharField(max_length=255, unique=True)
+    checked_out_books = models.ManyToManyField(Book, blank=True)
+    library_card_number = models.CharField(default=random_card_number)
+    
