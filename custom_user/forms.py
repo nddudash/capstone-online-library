0f0596b83b6a9bd1a64d3318e683f74f5b8f7bbb
Ryan Detzler <detzlerryan@gmail.com>
2021-10-05 14:01:12 -0400

    Forms were identical, combined into a single form

--- custom_user/forms.py
+++ custom_user/forms.py
@@ -1,10 +1,7 @@
 from django import forms
 from models import CustomUser
 
-class AddUserForm(forms.Form):
+class UserForm(forms.Form):
     username = forms.CharField(max_length=255)
     password = forms.CharField(widget=forms.PasswordInput)
 
-class LoginForm(forms.Form):
-    username = forms.CharField(max_length=255)
-    password = forms.CharField(widget=forms.PasswordInput)
