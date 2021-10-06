07d99daa29218fc1d135e13cf92f29c85c7e33ff
CodingBgirl <bgirlapollo@gmail.com>
2021-10-06 11:08:51 -0400

    Book Detail view completed

--- book/views.py
+++ book/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+from book.models import Book
+
 # Create your views here.
+def book_detail(request,id):
+  template_name = 'book_detail.html'
+  book = Book.objects.get(id=id)
+  context = {'book': book}
+  return render(request, template_name, context)
--- config/urls.py
+++ config/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from book.views import book_detail
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('bookdetail/<int:id>/', book_detail, name='bookdetail'),
 ]
