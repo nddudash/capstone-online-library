b781d81e3b45bbf33055f5a1d643bb32744681ce
Ryan Detzler <detzlerryan@gmail.com>
2021-10-06 14:18:24 -0400

    checkout_book_view modified so that the number of copies available is reduced, and a user cannot checkout a book if they already have it

--- checkout/views.py
+++ checkout/views.py
@@ -4,10 +4,11 @@ from book.models import Book
 
 def checkout_book_view(request, book_id):
     checkout_user = CustomUser.objects.get(username=request.user.username)
-    book = Book.objects.get(id=book_id)
+    book = Book.objects.get(gutenberg_id__exact=book_id)
 
-    if book.copies_available:
-        book.copies_available -= 1
+    if book.copies_available and not book in checkout_user.checked_out_books.all():
+        book.copies_available += -1
+        book.save()
         checkout_user.checked_out_books.add(book)
 #   This will redirect to the home page or user detail page, once they are finished
         return HttpResponse('You have successfully checked out this book')
