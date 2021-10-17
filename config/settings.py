cfdf15fa785208ee29196d34a393f81525c7208b
Nicholas Dudash <nddudash@gmail.com>
2021-10-17 17:36:50 -0600

    Deployment Setup

--- config/settings.py
+++ config/settings.py
@@ -145,3 +145,7 @@ LOGIN_URL = "/login_view/"
 # Registering Paths to store User Uploaded Images
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+# Configuration for Deployment, using Django-Heroku
+import django_heroku
+django_heroku.settings(locals())
